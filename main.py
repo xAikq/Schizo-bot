ee727d85b3b84e1128ba469ba20bc3ed18828883
Aik <xaikq@mail.ru>
2022-09-14 22:16:35 +0300

    initial gay test

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ load_dotenv(find_dotenv())
 bot = Bot(os.getenv('token'))
 dp = Dispatcher(bot)
 
+@dp.message_handler()
+async def gay_test():
+    min = 0
+    max = 100
+    await message.reply('Я гей на: ' + randit(min, max) + '%')
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
