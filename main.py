e900b39481bb64feb1f21deb7567f5f5000e1ddf
Aik <xaikq@mail.ru>
2022-09-14 22:38:35 +0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv, find_dotenv
 import os
 from random import randint
@@ -12,7 +12,8 @@ dp = Dispatcher(bot)
 async def gay_test(message):
     min = 0
     max = 100
-    await message.reply('Я гей на: ' + randit(min, max) + '%')
+    overall = str(randint(min, max))
+    await message.reply('Я гей на: ' + overall + '%')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
