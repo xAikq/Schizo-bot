6c72e4c3a8cce3268158ba3e828b5f01ec4d6012
Aik <xaikq@mail.ru>
2022-09-14 22:24:41 +0300

    initial gay reply

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ dp = Dispatcher(bot)
 async def gay_test(message):
     min = 0
     max = 100
-    await message.reply('Я гей на: ' + randit(min, max) + '%')
+    overall = str(randint(min, max))
+    await message.reply('Я гей на: ' + overall + '%')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
