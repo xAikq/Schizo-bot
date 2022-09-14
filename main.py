c1010011cb3a11f2ba3c57f440860435789ccd41
Aik <xaikq@mail.ru>
2022-09-14 22:06:39 +0300

    initial bot's token

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+
+bot = Bot(os.getenv('token'))
+dp = Dispatcher(bot)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
