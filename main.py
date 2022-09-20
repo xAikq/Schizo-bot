1f4550394132bc9c1ef24dda1787150083416372
Aik <xaikq@mail.ru>
2022-09-21 02:00:48 +0300

    initial toaster inline button

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ load_dotenv(find_dotenv())
 bot = Bot(os.getenv('token'))
 dp = Dispatcher(bot)
 
+toaster_result = {'0': 'Нихуя ты не тостер',
+                  '1': 'Нихуя ты не тостер',
+                  '2': 'Нихуя ты не тостер',
+                  '3': 'Нихуя ты не тостер',
+                  '4': 'Нихуя ты не тостер',
+                  '5': 'Нихуя ты не тостер',
+                  '6': 'Нихуя ты не тостер',
+                  '7': 'Ты лох какой-то, а не тостер',
+                  '8': 'Ты киберспортивный тостер',
+                  '9': 'Ты обычный тостер'}
+
 def inline_gay_handler():
     min_gay = 0
     max_gay = 100
@@ -54,17 +65,17 @@ def inline_beer_handler():
     return beer_articles
 
 def inline_deadinside_handler():
-    min_beer = 0
-    max_beer = 100
-    overall_beer = randint(min_beer, max_beer)
-    if overall_beer == 0:
+    min_deadinside = 0
+    max_deadinside = 100
+    overall_deadinside = randint(min_deadinside, max_deadinside)
+    if overall_deadinside == 0:
         deadinside_text = 'Нихуя ты не дединсайд...'
-    if overall_beer > 0 and overall_beer < 30:
-        deadinside_text = '🤡 Я дединсайд на: ' + str(overall_beer) + '%'
-    if overall_beer >= 30 and overall_beer < 70:
-        deadinside_text = '😋Я дединсайд на: ' + str(overall_beer) + '%'
-    if overall_beer >= 70:
-        deadinside_text = '😎 Я дединсайд на: ' + str(overall_beer) + '%'
+    if overall_deadinside > 0 and overall_deadinside < 30:
+        deadinside_text = '🤡 Я дединсайд на: ' + str(overall_deadinside) + '%'
+    if overall_deadinside >= 30 and overall_deadinside < 70:
+        deadinside_text = '😋Я дединсайд на: ' + str(overall_deadinside) + '%'
+    if overall_deadinside >= 70:
+        deadinside_text = '😎 Я дединсайд на: ' + str(overall_deadinside) + '%'
     result_deadinside_id: str = hashlib.md5(deadinside_text.encode()).hexdigest()
 
     deadinside_articles = [InlineQueryResultArticle(
@@ -75,12 +86,25 @@ def inline_deadinside_handler():
     input_message_content = types.InputTextMessageContent(message_text = deadinside_text))]
     return deadinside_articles
 
-result = inline_gay_handler() + inline_beer_handler() + inline_deadinside_handler()
+def inline_toaster_handler():
+        min_toaster = 0
+        max_toaster = 9
+        overall_toaster = str(randint(min_toaster, max_toaster))
+        toaster_text = toaster_result.get(f'{overall_toaster}')
+        result_toaster_id: str = hashlib.md5(toaster_text.encode()).hexdigest()
+
+        toaster_articles = [InlineQueryResultArticle(
+        id = result_toaster_id,
+        title = 'Насколько ты тостер',
+        description = 'Чё, тостер дохуя?',
+        thumb_url = 'https://headtopics.com/images/2019/3/18/lentaruofficial/105010861085109120479-1107539910196674560.webp?w=1280&h=853&q=1',
+        input_message_content = types.InputTextMessageContent(message_text = toaster_text))]
+        return toaster_articles
 
 @dp.inline_handler()
 async def inline_handler(query: types.InlineQuery):
-    articles = result
-    await query.answer(results = articles, cache_time = 1, is_personal = True)
+    articles = inline_gay_handler() + inline_beer_handler() +  inline_deadinside_handler() + inline_toaster_handler()
+    await query.answer(results = articles, cache_time = 60, is_personal = True)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
